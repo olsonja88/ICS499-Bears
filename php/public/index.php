4f3ea18b7a40089754d11d0a187797d0756fca52
olsonja88 <olsonja88@gmail.com>
2025-02-01 13:22:03 -0600

    feat: init php project

---
+++ php/public/index.php
@@ -0,0 +1,8 @@
+<?php
+include '../templates/header.php';
+?>
+    <h1>Welcome to My PHP Project</h1>
+    <p>This page is rendered using PHP templating.</p>
+<?php
+include '../templates/footer.php';
+?>
