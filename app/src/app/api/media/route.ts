3a3f66fe1ad86b6e516b123fe216bffd87c1f01f
olsonja88 <olsonja88@gmail.com>
2025-02-14 10:05:07 -0600

    refactor: API routes to be standardized with simple GET request written the same way

--- app/src/app/api/media/route.ts
+++ app/src/app/api/media/route.ts
@@ -1,17 +1,15 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import pool from '@/lib/db';
+import { NextResponse } from 'next/server';
+import { getDB } from '@/lib/db';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'GET') {
-        try {
-            const [rows]: any = await pool.query("SELECT * FROM media");
-            res.status(200).json(rows);
-        } catch (error) {
-            console.error("Database error:", error);
-            res.status(500).json({ success: false, error: (error as Error).message });
-        }
-    } else {
-        res.setHeader('Allow', ['GET']);
-        res.status(405).end(`Method ${req.method} Not Allowed`);
+export async function GET() {
+    try {
+        const db = await getDB();
+        const media = await db.all("SELECT * FROM media");
+        return NextResponse.json(media);
+    } catch (error) {
+        return NextResponse.json(
+            {error: "Database error", details: error },
+            { status: 500 }
+        );
     }
 }
